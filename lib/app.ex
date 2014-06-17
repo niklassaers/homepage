80da10242b55ddde5e56a3cd9f3f1753727fd893
Niklas Saers <niklas@saers.com>
2014-06-17 21:06:14 +0200

    Adapt routes

--- lib/app.ex
+++ lib/app.ex
@@ -3,6 +3,13 @@ defmodule Homepage do
   require Weber.Templates.ViewsLoader
 
   def start(_type, _args) do
+
+    blogPid = spawn(fn -> Blog.startup() end)
+    Process.register(blogPid, :blog)
+        
+    voterPid = spawn(fn -> PopularityVoter.startup() end)
+    Process.register(voterPid, :voter)
+
     # Set resources
     Weber.Templates.ViewsLoader.set_up_resources(File.cwd!)
     # compile all views
--- lib/route.ex
+++ lib/route.ex
@@ -4,5 +4,15 @@ defmodule Route do
   require Weber.Route
 
   route on("GET", "/", :Homepage.Main, :index)
+     |> on("GET", "/CV.html", :CV.Plain, :index)
+     |> on("GET", "/cv-portfolio.html", :CV.Portfolio, :index)
+     |> on("GET", "/contact.html", :Contact.Main, :index)
+
+     |> on("GET", "/blog", :Blog.Redirect, :index)
+     |> on("GET", "/rss", :Blog.RSS, :index)
+     |> on("GET", "/blog/:pageno", :Blog.Index, :index)
+     |> on("GET", "/archives/:year/:month/:day/:slug", :Blog.entry, :index)
+     #|> on(404)
+
 
 end
